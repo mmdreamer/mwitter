c24d535334fbdce301fa81f86efb92e49ad9aa0a
Miri <cindymr91@gmail.com>
2024-01-12 16:57:24 +1100

    Route setting done

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./routes/home";
 import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import "./styles/reset.css";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Layout />,
-
 		children: [
 			{
 				path: "",
@@ -19,11 +22,32 @@ const router = createBrowserRouter([
 			},
 		],
 	},
+	//Login 안했을때
+	{
+		path: "login",
+		element: <Login />,
+	},
+	{
+		path: "create-account",
+		element: <CreateAccount />,
+	},
 ]);
+
+const GlobalStyles = createGlobalStyle`
+  *{
+	box-sizing:border-box
+	}
+  body{
+	background-color: #000;
+	color:#fff;
+	font-family: 'Noto Sans KR', sans-serif;
+  }
+`;
 
 function App() {
 	return (
 		<>
+			<GlobalStyles />
 			<RouterProvider router={router} />
 		</>
 	);
