c85eb50c78d7fefdc8268d6f247402fac71b5fee
Miri <cindymr91@gmail.com>
2024-01-18 14:56:50 +1100

    Upload file

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,7 +1,8 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, updateDoc } from "firebase/firestore";
 import { useState } from "react";
 import { styled } from "styled-components";
-import { auth, db } from "../firebase";
+import { auth, db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const Form = styled.form`
 	display: flex;
@@ -67,8 +68,10 @@ export default function PostTweetForm() {
 	};
 	const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const { files } = e.target;
-		if (files && files.length === 1) {
+		if (files && files.length === 1 && files[0].size < 1024 * 1024) {
 			setFile(files[0]);
+		} else {
+			setFile(null); //1MB가 넘거나, 파일이 없을때..
 		}
 	};
 
@@ -79,12 +82,26 @@ export default function PostTweetForm() {
 
 		try {
 			setLoading(true);
-			await addDoc(collection(db, "tweets"), {
+			const doc = await addDoc(collection(db, "tweets"), {
 				tweet,
 				createdAt: Date.now(),
 				username: user.displayName || "Anonymous",
 				userId: user.uid,
 			});
+			if (file) {
+				const locationRef = ref(
+					storage,
+					`tweets/${user.uid}-${user.displayName}/${doc.id}`
+				);
+				const result = await uploadBytes(locationRef, file);
+				const url = await getDownloadURL(result.ref);
+				await updateDoc(doc, {
+					photo: url,
+				});
+			}
+			//Reset after submitting a tweet
+			setTweet("");
+			setFile(null);
 		} catch (e) {
 			console.log(e);
 		} finally {
@@ -95,6 +112,7 @@ export default function PostTweetForm() {
 	return (
 		<Form onSubmit={onSubmit}>
 			<TextArea
+				required
 				onChange={onChange}
 				rows={4}
 				maxLength={180}
